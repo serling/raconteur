f6763181db8cd0443b3b71c7662410fe2ed667c8
Thomas Christoffersen <t.christoffersen@creuna.no>
2019-07-31 13:09:36 +0200

    added missing meta

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ export default class MyDocument extends Document {
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
+          <meta name="description" content="improv resources" />
           <style>{``}</style>
         </Head>
         <body>
