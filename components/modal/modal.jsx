057803931f0a0af0ffc480f152979d314cb10f33
Thomas Christoffersen <t.christoffersen@creuna.no>
2019-08-01 11:06:48 +0200

    modal inline

--- components/modal/modal.jsx
+++ components/modal/modal.jsx
@@ -4,7 +4,6 @@ import ReactModal from 'react-modal';
 import cn from 'classnames';
 
 import Button from '../button/button';
-import './modal.scss';
 
 ReactModal.setAppElement('#__next');
 
@@ -30,34 +29,121 @@ class Modal extends React.Component {
 
   render() {
     return (
-      <ReactModal
-        isOpen={this.props.isVisible}
-        className={cn('modal', {
-          [`modal--${themes[this.props.theme]}`]: themes[this.props.theme]
-        })}
-        onRequestClose={this.props.onClose}
-        contentLabel={this.props.contentLabel}
-        shouldCloseOnOverlayClick={this.props.isCloseable}
-        overlayClassName={cn('modal__overlay', {
-          [`modal__overlay--${themes[this.props.theme]}`]: themes[
-            this.props.theme
-          ]
-        })}
-        closeTimeoutMS={200}
-      >
-        <div className="modal__content">
-          {this.props.isCloseable && (
-            <Button
-              textIsHidden={true}
-              onClick={this.props.onClose}
-              text="close modal"
-              iconName="close"
-              className="modal__close"
-            />
-          )}
-          {this.props.children}
-        </div>
-      </ReactModal>
+      <>
+        <ReactModal
+          isOpen={this.props.isVisible}
+          className={cn('modal', {
+            [`modal--${themes[this.props.theme]}`]: themes[this.props.theme]
+          })}
+          onRequestClose={this.props.onClose}
+          contentLabel={this.props.contentLabel}
+          shouldCloseOnOverlayClick={this.props.isCloseable}
+          overlayClassName={cn('modal__overlay', {
+            [`modal__overlay--${themes[this.props.theme]}`]: themes[
+              this.props.theme
+            ]
+          })}
+          closeTimeoutMS={200}
+        >
+          <div className="modal__content">
+            {this.props.isCloseable && (
+              <Button
+                textIsHidden={true}
+                onClick={this.props.onClose}
+                text="close modal"
+                iconName="close"
+                className="modal__close"
+              />
+            )}
+            {this.props.children}
+          </div>
+        </ReactModal>
+        <style jsx global>
+          {`
+            .modal {
+              $self: &;
+              position: absolute;
+              background-color: white;
+
+              &--menu {
+                width: 100%;
+                min-height: 100vh;
+                left: 0;
+                right: 0;
+                top: 0;
+                bottom: 0;
+
+                #{$self}__content {
+                  max-width: 1240px;
+                  margin: 0 auto;
+                  padding: 2rem 2rem 2rem 2rem;
+                  position: relative;
+                }
+
+                #{$self}__close {
+                  top: 2rem;
+                }
+              }
+
+              &--prompt {
+                left: 2rem;
+                right: 2rem;
+                top: 50%;
+                transform: translateY(-50%);
+                box-shadow: 10px 10px 5px -9px rgba(0, 0, 0, 0.75);
+                border: 1px solid #ececec;
+
+                #{$self}__content {
+                  padding: 4rem 2rem 2rem 2rem;
+                  position: relative;
+                }
+              }
+
+              &__close {
+                position: absolute;
+                top: 2rem;
+                right: 2rem;
+              }
+
+              &__overlay {
+                position: fixed;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background-color: rgba(255, 255, 255, 0.75);
+                z-index: 11;
+
+                &--prompt {
+                  opacity: 0;
+                  transition: opacity 0.2s ease-in-out;
+
+                  &.ReactModal__Overlay--after-open {
+                    opacity: 1;
+                  }
+
+                  &.ReactModal__Overlay--before-close {
+                    opacity: 0;
+                  }
+                }
+
+                &--menu {
+                  transform: translateX(100%);
+                  transition: transform 0.2s ease-in-out;
+
+                  &.ReactModal__Overlay--after-open {
+                    transform: translateX(0);
+                  }
+
+                  &.ReactModal__Overlay--before-close {
+                    transform: translateX(100%);
+                  }
+                }
+              }
+            }
+          `}
+        </style>
+      </>
     );
   }
 }
