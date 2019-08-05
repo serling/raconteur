185725604e99189023066876bfe34ec4804c8c00
Thomas Christoffersen <t.christoffersen@creuna.no>
2019-08-05 11:44:19 +0200

    analyzer config

--- next.config.js
+++ next.config.js
@@ -37,14 +37,7 @@ const bundleAnalyzerConfig = {
   analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
   analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
   bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/server.html'
-    },
-    browser: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/client.html'
-    }
+    enabled: process.env.ANALYZE === 'true'
   }
 };
 
