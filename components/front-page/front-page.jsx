0ae6c19f0ef7e7b024c11f1ae135cbc3baf4ca4a
Thomas Christoffersen <t.christoffersen@creuna.no>
2019-08-13 20:26:27 +0200

    grid stuff

--- components/front-page/front-page.jsx
+++ components/front-page/front-page.jsx
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types';
 import Layout from '../layout/layout';
 import PageTitle from '../page-title/page-title';
 import ArticlesList from '../articles-list/articles-list';
+import Grid from '../grid/grid';
+import Article from '../articles-list/article';
 
 class FrontPage extends React.Component {
   static pageTransitionDelayEnter = true;
 
   state = { hasLoaded: false };
+
+  //TODO: design - https://css-tricks.com/snippets/css/complete-guide-grid/
 
   componentDidMount() {
     this.setState(
@@ -30,6 +34,19 @@ class FrontPage extends React.Component {
       <Layout title={pageTitle}>
         <PageTitle text={title} />
         <ArticlesList articles={articles} />
+        <Grid>
+        {/* {articles.map(article => (
+          <Article {...article} />
+        ))} */}
+          <div>hello</div>
+          <div>I am block</div>
+          <div>Third thing</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+        </Grid>
       </Layout>
     );
   }
--- components/grid/grid.jsx
+++ components/grid/grid.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 
-const Grid = ({ children }) => {
+import PageContent from '../page-content/page-content';
+
+const themes = {
+  default: "default",
+  uniform: "uniform"
+};
+
+const Grid = ({ children, theme }) => {
   return (
       <>
-    <div className="grid">
+      <PageContent>
+    <div className={cn("grid", {
+      [`grid--${themes[theme]}`]: themes[theme]
+    })}>
       {React.Children.map(children, (child, index) => {
         return (
             <div className={cn("grid-item", `grid__item--${index}`)} key={index}>
@@ -14,21 +24,71 @@ const Grid = ({ children }) => {
         );
       })}
     </div>
+    </PageContent>
     <style jsx>
     {`
       .grid {
+        display: grid;
+        grid-template-rows: auto;
+        grid-column-gap: 0.5rem;
+        grid-row-gap: 0.5rem;
 
-          &__item {
-          background-color: red;
-          }
+        &--default {
+          grid-template-columns: 1fr 1fr 1fr 1fr;
+          grid-template-areas: 
+            "first first first second"
+            "third third fourth fourth"
+            "fifth sixth seventh eighth";
+        }
 
-          &__item--0 {
-              background-color: blue;
-          }
+        &--uniform {
+          
+        }
+        
+        &__item {
+          background-color: gray;
+        }
 
-          &__item--1 {
-              background-color: yellow;
-          }
+        &__item--0 {
+          grid-area: first;
+          background-color: blue;
+        }
+
+        &__item--1 {
+          grid-area: second;
+          background-color: yellow;
+        }
+
+        &__item--2 {
+          grid-area: third;
+          background-color: cyan;
+        }
+
+        &__item--3 {
+          grid-area: fourth;
+          background-color: purple;
+        }
+
+        &__item--4 {
+          grid-area: fifth;
+          background-color: magenta;
+        }
+
+        &__item--5 {
+          grid-area: sixth;
+          background-color: black;
+          color: white;
+        }
+
+        &__item--6 {
+          grid-area: seventh;
+          background-color: green;
+        }
+
+        &__item--7 {
+          grid-area: eighth;
+          background-color: darkred;
+        }
       }
     `}
     </style>
@@ -37,15 +97,18 @@ const Grid = ({ children }) => {
 };
 
 Grid.propTypes = {
-    components: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string
-    })
-  ).isRequired
+  components: PropTypes.arrayOf(
+  PropTypes.shape({
+    id: PropTypes.string
+  })
+).isRequired
 };
 
 Grid.defaultProps = {
-    components: []
+  components: [],
+  theme: themes.default
 };
+
+Grid.themes = themes;
 
 export default Grid;
