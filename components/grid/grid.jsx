79eae985344a7def3e59c963534f10ad58daab4d
Thomas Christoffersen <t.christoffersen@creuna.no>
2019-08-14 14:01:22 +0200

    mqs

--- components/grid/grid.jsx
+++ components/grid/grid.jsx
@@ -30,95 +30,68 @@ const Grid = ({ children, theme }) => {
         $break-at-lg: 64rem; //1024px
 
         display: grid;
-        grid-template-rows: auto;
-        grid-column-gap: 0.5rem;
-        grid-row-gap: 0.5rem; 
+        grid-column-gap: 2rem;
+        grid-row-gap: 2rem; 
+        
         
         @media screen and (min-width: $break-at-md) {
+          grid-template-rows: auto;
+          grid-column-gap: 0.5rem;
+          grid-row-gap: 0.5rem; 
         }
 
         &--default {
-          grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-
-        #{$self}__item--0 {
-          grid-row: 1 / span 2;
-          grid-column: 1 / span 6;
-          position: relative;
-
-          &::after {
-            display: block;
-            width: 0;
-            height: 0;
-            border: 0 solid transparent;
-            border-right-color: #f9f8f3fc;
-            border-width: 0 2rem 2rem 0;
-            content: "";
-            position: absolute;
-            right: 0;
-            top: 0;
-            z-index: 5;
+          @media screen and (min-width: $break-at-md) {
+            grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
           }
-        }
 
-        #{$self}__item--1 {
-          grid-row: 1 / span 2;
-          grid-column: 7 / span 3;
-          padding-top: 2rem;
-        }
+          #{$self}__item--0 {
+            @media screen and (min-width: $break-at-md) {
+            grid-row: 1 / span 2;
+            grid-column: 1 / span 6;
+            position: relative;
 
-        #{$self}__item--2 {
-          grid-column: 1 / span 3;
-        }
+              &::after {
+                display: block;
+                width: 0;
+                height: 0;
+                border: 0 solid transparent;
+                border-right-color: #f9f8f3fc;
+                border-width: 0 2rem 2rem 0;
+                content: "";
+                position: absolute;
+                right: 0;
+                top: 0;
+                z-index: 5;
+              }
+            }
+          }
 
-        #{$self}__item--3 {
-          grid-column: 4 / span 3;
-        }
+          #{$self}__item--1 {
+            @media screen and (min-width: $break-at-md) {
+            grid-row: 1 / span 2;
+            grid-column: 7 / span 3;
+            padding-top: 2rem;
+            }
+          }
 
-        #{$self}__item--4 {
-          grid-column: 7 / span 3;
-        }
+          #{$self}__item--2 {
+            @media screen and (min-width: $break-at-md) {
+            grid-column: 1 / span 3;
+            }
+          }
 
-        #{$self}__item--5 {
-          
-        }
+          #{$self}__item--3 {
+            @media screen and (min-width: $break-at-md) {
+            grid-column: 4 / span 3;
+            }
+          }
 
-        #{$self}__item--6 {
-        }
-
-        #{$self}__item--7 {
-        }
-          
-        }
-
-        &--uniform {
-          
-        }
-        
-        &__item {
-        }
-
-        &__item--0 {
-        }
-
-        &__item--1 {
-        }
-
-        &__item--2 {
-        }
-
-        &__item--3 {
-        }
-
-        &__item--4 {
-        }
-
-        &__item--5 {
-        }
-
-        &__item--6 {
-        }
-
-        &__item--7 {
+          #{$self}__item--4 {
+            @media screen and (min-width: $break-at-md) {
+            grid-column: 7 / span 3;
+            }
+          }
         }
       }
     `}
