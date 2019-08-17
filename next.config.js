e3c7a8377e07185e3126bab95f5af410c24d0203
Thomas Christoffersen <t.christoffersen@creuna.no>
2019-08-17 12:28:18 +0200

    unsed packages

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const withSass = require('@zeit/next-sass');
 
 const withPlugins = require('next-compose-plugins');
 const withFonts = require('next-fonts');
-const withSize = require('next-size');
-const withProgressBar = require('next-progressbar');
 const withOptimizedImage = require('next-optimized-images');
 
 const nextConfig = {
@@ -26,20 +24,12 @@ const optimizedImageConfig = {
   handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif']
 };
 
-const progressBarConfig = {
-  progressBar: {
-    // profile: true
-  }
-};
-
 const fontsConfig = {
   enableSvg: true
 };
 
 const plugins = [
-  withSize,
   [withSass, SassConfig],
-  [withProgressBar, progressBarConfig],
   [withFonts, fontsConfig],
   [withOptimizedImage, optimizedImageConfig]
 ];
